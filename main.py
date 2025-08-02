e7dda16bf3db26ee91c2fc9d3fe25ba10e4a3894
Luka-group-64 <lukagurgenidzegoa64@gmail.com>
2025-08-02 21:34:03 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+print("I am team captain")
+print("I wrote base code")
+
+def main():
+    while True:
+        print("Welcome")
+        print("1 - Register")
+        print("2 - Login")
+        print("3 - Exit")
+main()
